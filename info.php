6e82fdeb71186dec2da000b9be77b8ab0e19b98c
Andre <[andre_w3@yahoo.com]>
2018-08-27 18:39:12 -0700

    php learning

---
+++ info.php
@@ -0,0 +1,16 @@
+<?php
+// for ($fee = 5; $fee <= 1000; $fee=$fee+5)
+// echo "$fee <br>";
+?>
+
+<?php
+$num = 5; $i = 0;
+for ($num = 5; $num < 100; $num + 5) {
+  $i += $num;
+}
+// for ($num = 5; $num < 100; $num )
+
+ ?>
+
+
+ 
